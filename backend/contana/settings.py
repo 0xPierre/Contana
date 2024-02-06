123ee94683c77b366d469047696a0817890fa9f8
0xPierre <0xpierre@protonmail.com>
2024-02-06 11:54:12 +0100

    ci: update from AWS to OVH S3

--- backend/contana/settings.py
+++ backend/contana/settings.py
@@ -172,11 +172,13 @@ SIMPLE_JWT = {
 
 
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_S3_BUCKET_NAME")
-AWS_S3_REGION_NAME = "eu-west-3"
-AWS_S3_ACCESS_KEY_ID = os.environ.get("AWS_S3_ACCESS_KEY_ID")
-AWS_S3_SECRET_ACCESS_KEY = os.environ.get("AWS_S3_SECRET_ACCESS_KEY")
-AWS_QUERYSTRING_AUTH = False
+AWS_STORAGE_BUCKET_NAME = os.environ.get("S3_BUCKET_NAME")
+AWS_S3_REGION_NAME = os.environ.get("S3_REGION_NAME")
+AWS_S3_ACCESS_KEY_ID = os.environ.get("S3_ACCESS_KEY")
+if os.environ.get("S3_ENDPOINT"):
+    AWS_S3_ENDPOINT_URL = os.environ.get("S3_ENDPOINT")
+AWS_S3_SECRET_ACCESS_KEY = os.environ.get("S3_SECRET_KEY")
+AWS_QUERYSTRING_AUTH = True
 
 
 EMAIL_BACKEND = "django_ses.SESBackend"
