ff5a6591b11424c5408ca58b2c298b66300f699b
0xPierre <0xpierre@protonmail.com>
2024-02-22 18:37:16 +0100

    fix: remove frontend from docker compose

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,10 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 5173
+    port: 5173,
+    watch: {
+      usePolling: true
+    }
   },
   plugins: [
     vue({
