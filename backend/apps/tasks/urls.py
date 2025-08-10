4aadd541fb40ba36811193af5fed7d8160c4e024
0xPierre <0xpierre@protonmail.com>
2025-08-10 18:26:34 +0200

    fix: basename router

--- backend/apps/tasks/urls.py
+++ backend/apps/tasks/urls.py
@@ -4,8 +4,8 @@ from rest_framework.routers import DefaultRouter
 from . import views
 
 router = DefaultRouter()
-router.register(r"labels", views.LabelViewset, basename="clients")
-router.register(r"", views.TaskViewset, basename="clients")
+router.register(r"labels", views.LabelViewset, basename="labels")
+router.register(r"", views.TaskViewset, basename="tasks")
 
 urlpatterns = [
     path("", include(router.urls)),
